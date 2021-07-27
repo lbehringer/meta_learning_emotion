6ec0ed93902742d397e5f3a701751d990096bb8a
Vanessa Dengel <dengelva@adler.ims.uni-stuttgart.de>
2021-07-27 11:41:32 +0200

    added cosine similarity

--- loss.py
+++ loss.py
@@ -12,7 +12,7 @@ class ContrastiveLoss(torch.nn.Module):
     label = 0 if similar 
     """
 
-    def __init__(self, margin=2.0):
+    def __init__(self, margin):
         super(ContrastiveLoss, self).__init__()
         self.margin = margin
 
@@ -24,6 +24,27 @@ class ContrastiveLoss(torch.nn.Module):
         return loss_contrastive
 
 
+
+
+class ContrastiveLossCosine(torch.nn.Module):
+    """
+    Contrastive loss function.
+    Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    label = 1 if dissimilar
+    label = 0 if similar 
+    """
+
+    def __init__(self):
+        super(ContrastiveLossCosine, self).__init__()
+        #self.margin = margin
+
+    def forward(self, output1, output2, label):
+        cos = nn.CosineSimilarity(dim=1, eps=1e-6)
+        cosine_similarity = cos(output1, output2)
+
+        loss_contrastive =  (1-label) * 1/2 * torch.pow(1-cosine_similarity, 2) + label * 1/2 * torch.pow(cosine_similarity, 2) 
+
+        return loss_contrastive
 
 
 

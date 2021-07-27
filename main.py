1a7d994220a866809dd805823e2cc7e46e077ef1
Vanessa Dengel <dengelva@adler.ims.uni-stuttgart.de>
2021-07-27 11:40:24 +0200

    updated + Siamese

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 import numpy as np
-from input_features import audio_to_spectrogram, load_dvector, concatenate_features
-from model import CNN_BLSTM_SELF_ATTN
+#from input_features import audio_to_spectrogram
+from model import CNN_BLSTM_SELF_ATTN, Siamese
 from dataset import EmotionDataset, create_train_test
 import torch
 from torch.utils.data.dataset import random_split
@@ -11,41 +11,44 @@ from embeddings2json import get_embeddings
 
 def main(args):
     # load dataset, split into train & test
-    dataset = EmotionDataset('data/pavoque/pavoque_all_500_dur_7_5_norm_0to1.json')
-    support_data = EmotionDataset('data/pavoque/support_0.json')
+    dataset = EmotionDataset(
+        'data/pavoque/pavoque_across_500_dur_4_preemph_norm_0to1.json')
+    support_data = EmotionDataset(
+        'data/pavoque/pavoque_across_500_dur_4_preemph_support_norm_0to1.json')
 
     query_set, train1, train2 = create_train_test(dataset)
-    support_set = torch.utils.data.DataLoader(support_data,batch_size=1,
-                                                   shuffle= False)
+    support_set = torch.utils.data.DataLoader(support_data, batch_size=1,
+                                              shuffle=False)
 
-   
-
+    # choose model
     model = CNN_BLSTM_SELF_ATTN(args.input_spec_size, args.cnn_filter_size, args.num_layers_lstm,
                                 args.num_heads_self_attn, args.hidden_size_lstm, args.num_emo_classes, args.num_gender_class, args.embedding_size, args.n_mels)
 
-    #model.cuda()
+    #model = Siamese()
+
+    #model = EmotionClassificationNet()
+    # model.cuda()
 
     # train & evaluate model
     if args.train:
-        train(model, args.num_epochs, train1, train2)
+        train(model, args.num_epochs, train1, train2, query_set, support_set)
 
     if args.evaluate:
-        evaluate(model, support_set, query_set, "state_dict_model_margin.pt")
+        evaluate(model, support_set, query_set, args.trained_model)
 
     # get embeddings
     if args.embeddings2file:
-        get_embeddings(model, "state_dict_model_margin.pt", query_set)
-
+        get_embeddings(model, args.trained_model, query_set)
 
     # visualization (t-sne, PCA)
-    #tsne.py
+    # tsne.py
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--offset", default=0, required=False,
                         type=int, help="offset in seconds")
-    parser.add_argument("--duration", default=7.5, required=False,
+    parser.add_argument("--duration", default=4, required=False,
                         type=int, help="window size in seconds")
     parser.add_argument("--n_mels", default=26, required=False,
                         type=int, help="number of filterbanks")
@@ -65,14 +68,16 @@ if __name__ == "__main__":
                         required=False, type=int, help="gender classes --> m & f")
     parser.add_argument("--embedding_size", default=150,
                         required=False, type=int, help="embedding size for emotion embeddings")
-    parser.add_argument("--num_epochs", default=120,
+    parser.add_argument("--num_epochs", default=15,
                         required=False, type=int, help="num_epochs")
     parser.add_argument("--embeddings2file", default=True,
                         required=False, type=bool, help="write embeddings to file?")
-    parser.add_argument("--train", default=False,
+    parser.add_argument("--train", default=True,
                         required=False, type=bool, help="train model?")
-    parser.add_argument("--evaluate", default=False,
+    parser.add_argument("--evaluate", default=True,
                         required=False, type=bool, help="evaluate model?")
+    parser.add_argument("--trained_model", default="state_dict_model_siamese.pt",
+                        required=False, type=str, help="load trained model parameters")
 
     args = parser.parse_args()
 
--- training.py
+++ training.py
@@ -4,18 +4,18 @@ from torch import optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 import numpy as np
 from sklearn.metrics import accuracy_score
-from loss import ContrastiveLoss
+from loss import ContrastiveLoss, ContrastiveLossCosine
 from model import CNN_BLSTM_SELF_ATTN
 from sklearn.metrics import f1_score
 import torch.nn.functional as F
 from scipy.spatial import distance
 
 
-def train(model, num_epochs, dataloader_train1, dataloader_train2):
-    model.load_state_dict(torch.load('state_dict_model_CUDA.pt'))
+def train(model, num_epochs, dataloader_train1, dataloader_train2, support, query):
+    #model.load_state_dict(torch.load('state_dict_model_marg4.pt')) # continue training with model 
     model.train()
     labels_emo_map = {'sad': 0, 'ang': 1, 'pok': 2, 'hap': 3, 'neu': 4}
-    model = model 
+   
     #if torch.cuda.is_available():
     #    device = torch.device("cuda")
     #    print('CUDA available')
@@ -25,7 +25,8 @@ def train(model, num_epochs, dataloader_train1, dataloader_train2):
 
     optimizer = optim.Adam(model.parameters(), lr=0.001,
                            weight_decay=1e-05, betas=(0.9, 0.98), eps=1e-9)
-    loss = ContrastiveLoss(2)
+    criterion = ContrastiveLoss(4)
+    #criterion = ContrastiveLossCosine()
    
     scheduler = ReduceLROnPlateau(
         optimizer, mode='min', patience=3, factor=0.1, verbose=True)
@@ -50,6 +51,7 @@ def train(model, num_epochs, dataloader_train1, dataloader_train2):
             spec_features1 = sample1[0]
             spec_features2 = sample2[0]
            
+           
        
             spec_features1, spec_features2, label_emo1, label_emo2 = spec_features1.to(
                 device, dtype=torch.float), spec_features2.to(
@@ -73,13 +75,13 @@ def train(model, num_epochs, dataloader_train1, dataloader_train2):
             emotion_embedding2 = torch.unsqueeze(emotion_embedding2, 0)
           
             # calculate loss
-            _loss = loss(emotion_embedding1, emotion_embedding2, label)
+            loss = criterion(emotion_embedding1, emotion_embedding2, label)
 
-            _loss.backward()
+            loss.backward()
             # update model weights
             optimizer.step()
 
-            train_loss_list.append(_loss.item())
+            train_loss_list.append(loss.item())
 
             if i % 500 == 0:
                 print('Loss {} after {} iterations'.format(
@@ -91,17 +93,20 @@ def train(model, num_epochs, dataloader_train1, dataloader_train2):
                     np.mean(np.asarray(mean_loss)), epoch))
         
     
-        PATH = "state_dict_model_CUDA.pt"
+        PATH = "state_dict_model_20.pt"
         torch.save(model.state_dict(), PATH)
+        #evaluate(model, support, query, PATH)
+
+
+
 
 def evaluate(model, support, query, PATH):
     labels_emo_map = {'sad': 0, 'ang': 1, 'pok': 2, 'hap': 3, 'neu': 4}
-    #labels_gen_map = {'m': 0, 'f': 1}
+
     with torch.no_grad():
             model.load_state_dict(torch.load(PATH))
             model.double()
             model.eval()
-            loss = ContrastiveLoss(2)
             y_pred = list()
             y_true = list()
             for i, samp1 in enumerate(query):
@@ -121,19 +126,16 @@ def evaluate(model, support, query, PATH):
                         label = 0
                     spec_support = samp2[0]
                     emb1, emb2 = model(spec_query, spec_support)
-                    #print(f'support: {samp2[1]}')
 
                     #emb1 = torch.unsqueeze(emb1, 0)
                     #emb2 = torch.unsqueeze(emb2, 0)
             
-          
                     similarity = abs(distance.euclidean(emb1, emb2))
                    
                     #_loss = loss(emb1, emb2, label)
                     losses[int(label_emo2.item())] = similarity
-                    print(losses)
+                    #print(losses)
               
-                #print(losses)
                 prediction = min(losses, key=losses.get)
                 y_pred.append(prediction)
    
@@ -142,14 +144,3 @@ def evaluate(model, support, query, PATH):
             score = f1_score(y_true, y_pred, average='macro')
             print(f'f_score: {score}')
           
-
-
-
-
-
-
-#labels_gen_map = {'m': 0, 'f': 1}
-#labels_gen1 = torch.FloatTensor([float(labels_gen_map[label]) for label in sample1[2]])
-#labels_gen1 = torch.FloatTensor([float(labels_gen_map[label]) for label in samp1[2]])
-#labels_gen2 = torch.FloatTensor([float(labels_gen_map[label]) for label in sample2[2]])
-#labels_gen2 = torch.FloatTensor([float(labels_gen_map[label]) for label in sample2[2]])

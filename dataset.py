5558f82b8d43144be759fd32fc454a1ed9bb5de9
Vanessa Dengel <dengelva@kiwi.ims.uni-stuttgart.de>
2021-08-23 16:53:36 +0200

    added confusion matrix + plots

--- dataset.py
+++ dataset.py
@@ -6,9 +6,16 @@ from torch.utils.data.dataset import random_split
 
 
 iemocap_across = '/mount/arbeitsdaten/studenten1/team-lab-phonetics/2021/student_directories/Lyonel_Behringer/advanced-ml/iemocap_across_500_dur_4_spectrograms.json'
-iemocap_across_support = ""
+iemocap_across_support = '/mount/arbeitsdaten/studenten1/advanced_ml/dengelva/meta_learning_emotion/data/iemocap/iemocap_across_502_dur_4_support.json'
 pavoque_across = '/mount/arbeitsdaten/studenten1/advanced_ml/dengelva/meta_learning_emotion/data/pavoque/pavoque_across_500_dur_4_preemph_norm_0to1.json'
 pavoque_across_support = '/mount/arbeitsdaten/studenten1/advanced_ml/dengelva/meta_learning_emotion/data/pavoque/pavoque_across_500_dur_4_preemph_support_norm_0to1.json'
+pavoque_all = '/mount/arbeitsdaten/studenten1/advanced_ml/dengelva/meta_learning_emotion/data/pavoque/pavoque_all_500_dur_4_preemph_norm_0to1.json'
+pavoque_all_support = ""
+singapore_emo_en = '/mount/arbeitsdaten/studenten1/advanced_ml/dengelva/meta_learning_emotion/data/merged_en.json'
+singapore_emo_zh = '/mount/arbeitsdaten/studenten1/advanced_ml/dengelva/meta_learning_emotion/data/merged_zh.json'
+singapore_emo_en_support = '/mount/arbeitsdaten/studenten1/advanced_ml/dengelva/meta_learning_emotion/data/support_merged_en.json'
+singapore_emo_zh_support = '/mount/arbeitsdaten/studenten1/advanced_ml/dengelva/meta_learning_emotion/data/support_merged_zh.json'
+
 
 class EmotionDataset(Dataset):
     def __init__(self, file_name):
@@ -31,6 +38,7 @@ class EmotionDataset(Dataset):
         # access with name_of_dataset[idx]
         return self.spectrogram[idx], self.emotion_label[idx], self.gender_label[idx]
 
+
 def create_classification_set(dataset):
     split_train = int(len(dataset) * 9/10)
     split_test = len(dataset) - split_train
@@ -38,15 +46,14 @@ def create_classification_set(dataset):
     train, test = random_split(dataset=dataset, lengths=[
                                split_train, split_test])
     test_dataloader = torch.utils.data.DataLoader(test,
-                                                   batch_size=1,
-                                                   shuffle=False)
-    
+                                                  batch_size=1,
+                                                  shuffle=False)
+
     train_dataloader = torch.utils.data.DataLoader(train,
                                                    batch_size=16,
                                                    shuffle=True)
-    
-    return train_dataloader, test_dataloader
 
+    return train_dataloader, test_dataloader
 
 
 def create_train_test(dataset):
@@ -54,30 +61,20 @@ def create_train_test(dataset):
     split_train = int(len(dataset) * 9/10)
     split_test = len(dataset) - split_train
 
-   
-
     # split dataset in train and test
     train, test = random_split(dataset=dataset, lengths=[
                                split_train, split_test])
-    
 
     # load train and test data to batches and shuffle
     query_dataloader = torch.utils.data.DataLoader(test,
                                                    batch_size=1,
                                                    shuffle=False)
-    
+
     train_dataloader1 = torch.utils.data.DataLoader(train,
-                                                   batch_size=32,
-                                                   shuffle=True)
+                                                    batch_size=32,
+                                                    shuffle=True)
     train_dataloader2 = torch.utils.data.DataLoader(train,
-                                                   batch_size=32,
-                                                   shuffle=True)
-   
+                                                    batch_size=32,
+                                                    shuffle=True)
 
     return query_dataloader, train_dataloader1, train_dataloader2
-
-
-
-#dataset = EmotionDataset('data/pavoque/pavoque_across_500.json')
-#print(dataset[0])
-#print(create_train_test(dataset))
--- training.py
+++ training.py
@@ -9,13 +9,15 @@ from model import CNN_BLSTM_SELF_ATTN
 from sklearn.metrics import f1_score
 import torch.nn.functional as F
 from scipy.spatial import distance
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 
 def train(model, num_epochs, dataloader_train1, dataloader_train2, support, query, path):
-    model.load_state_dict(torch.load('state_dict_model_meta_pavoque_21_08_emb100_iemo_pre.pt')) # continue training with model 
+    #model.load_state_dict(torch.load('state_dict_model_meta_singapore_en_1200ep_emb150_batch32.pt')) # continue training with model
     model.train()
     labels_emo_map = {'sad': 0, 'ang': 1, 'hap': 2, 'neu': 3, 'pok': 4}
-   
+
     if torch.cuda.is_available():
         device = torch.device("cuda")
         print('CUDA available')
@@ -29,8 +31,8 @@ def train(model, num_epochs, dataloader_train1, dataloader_train2, support, quer
     # criterion = ContrastiveLoss(4)
     criterion = torch.nn.CosineEmbeddingLoss()
     # Measures the loss given an input tensor x and a labels tensor y (containing 1 or -1)
-   
-    #scheduler = ReduceLROnPlateau(
+
+    # scheduler = ReduceLROnPlateau(
     #    optimizer, mode='min', patience=3, factor=0.1, verbose=True)
 
     train_loss_list = []
@@ -39,10 +41,10 @@ def train(model, num_epochs, dataloader_train1, dataloader_train2, support, quer
             # categorical to numeric labels
             label_emo1 = torch.FloatTensor(
                 [float(labels_emo_map[label]) for label in sample1[1]])
-            
+
             label_emo2 = torch.FloatTensor(
                 [float(labels_emo_map[label]) for label in sample2[1]])
-            
+
             targets = list()
             for label1, label2 in zip(label_emo1, label_emo2):
                 if label1 == label2:
@@ -50,18 +52,16 @@ def train(model, num_epochs, dataloader_train1, dataloader_train2, support, quer
                 else:
                     targets.append(-1)
             targets = torch.FloatTensor(targets)
-     
-            #print(targets)
+
+            # print(targets)
             spec_features1 = sample1[0]
             spec_features2 = sample2[0]
-            #print(spec_features1.shape)
-           
-       
+            # print(spec_features1.shape)
+
             spec_features1, spec_features2, label_emo1, label_emo2, targets = spec_features1.to(
                 device, dtype=torch.float), spec_features2.to(
                 device, dtype=torch.float), label_emo1.to(device), label_emo2.to(device), targets.to(device)
-            
-            
+
             spec_features1.requires_grad = True
             spec_features2.requires_grad = True
 
@@ -70,17 +70,18 @@ def train(model, num_epochs, dataloader_train1, dataloader_train2, support, quer
 
             # model output
 
-            emotion_embedding1, emotion_embedding2 = model(spec_features1, spec_features2) # siamese
+            emotion_embedding1, emotion_embedding2 = model(
+                spec_features1, spec_features2)  # siamese
             #emotion_embedding1 = model(spec_features1)
             #emotion_embedding2 = model(spec_features2)
 
-            #print(emotion_embedding1)
+            # print(emotion_embedding1)
             #print(f'emotion embedding1: {emotion_embedding1}')
             #print(f'emotion embedding2: {emotion_embedding2}')
 
             #emotion_embedding1 = torch.unsqueeze(emotion_embedding1, 0)
             #emotion_embedding2 = torch.unsqueeze(emotion_embedding2, 0)
-          
+
             # calculate loss
             loss = criterion(emotion_embedding1, emotion_embedding2, targets)
 
@@ -93,58 +94,78 @@ def train(model, num_epochs, dataloader_train1, dataloader_train2, support, quer
             if i % 500 == 0:
                 print('Loss {} after {} iterations'.format(
                     np.mean(np.asarray(train_loss_list)), i))
-         
 
         mean_loss = np.mean(np.asarray(train_loss_list))
         print('Loss {} after {} epochs'.format(
-                    np.mean(np.asarray(mean_loss)), epoch))
-        
-    
+            np.mean(np.asarray(mean_loss)), epoch))
+
         torch.save(model.state_dict(), path)
         #evaluate(model, support, query, path)
 
 
-def evaluate(model, support, query, PATH):
+def evaluate(model, support, query, PATH, confusion_file):
     labels_emo_map = {'sad': 0, 'ang': 1, 'hap': 2, 'neu': 3, 'pok': 4}
+    labels_emo_map_reversed = {0: 'sad', 1: 'ang', 2: 'hap', 3: 'neu', 4: 'pok'}
 
     with torch.no_grad():
-            model.load_state_dict(torch.load(PATH))
-            model.double()
-            model.eval()
-            y_pred = list()
-            y_true = list()
-            for i, samp1 in enumerate(query):
-                #print(f'query: {samp1}')
-                label_emo1 = torch.FloatTensor([float(labels_emo_map[label]) for label in samp1[1]])
-                y_true.append(int(label_emo1.item()))
-                spec_query = samp1[0]
-        
-                losses = dict()
-                # compare sample in query set with every sample in emotion set and choose emotion with highest similarity 
-                for samp2 in support:
-                    label_emo2 = torch.FloatTensor([float(labels_emo_map[label]) for label in samp2[1]])
-                    
-                    if label_emo1 == label_emo2:
-                        label = 1
-                    else:
-                        label = 0
-                    spec_support = samp2[0]
-                    emb1, emb2 = model(spec_query, spec_support)
+        model.load_state_dict(torch.load(PATH))
+        model.double()
+        model.eval()
+        y_pred = list()
+        y_true = list()
+        for i, samp1 in enumerate(query):
+            #print(f'query: {samp1}')
+            label_emo1 = torch.FloatTensor(
+                [float(labels_emo_map[label]) for label in samp1[1]])
+            y_true.append(int(label_emo1.item()))
+            spec_query = samp1[0]
 
-                    #emb1 = torch.unsqueeze(emb1, 0)
-                    #emb2 = torch.unsqueeze(emb2, 0)
-            
-                    similarity = abs(distance.euclidean(emb1, emb2))
-                   
-                    #_loss = loss(emb1, emb2, label)
-                    losses[int(label_emo2.item())] = similarity
-                    #print(losses)
-              
-                prediction = min(losses, key=losses.get)
-                y_pred.append(prediction)
-   
-            print(f'predictions: {y_pred}')
-            print(f'gold label: {y_true}')
-            score = f1_score(y_true, y_pred, average='macro')
-            print(f'f_score: {score}')
-          
+            losses = dict()
+            # compare sample in query set with every sample in emotion set and choose emotion with highest similarity
+            for samp2 in support:
+                label_emo2 = torch.FloatTensor(
+                    [float(labels_emo_map[label]) for label in samp2[1]])
+
+                if label_emo1 == label_emo2:
+                    label = 1
+                else:
+                    label = 0
+                spec_support = samp2[0]
+                emb1, emb2 = model(spec_query, spec_support)
+
+                #emb1 = torch.unsqueeze(emb1, 0)
+                #emb2 = torch.unsqueeze(emb2, 0)
+
+                similarity = abs(distance.cosine(emb1, emb2))
+
+                #_loss = loss(emb1, emb2, label)
+                losses[int(label_emo2.item())] = similarity
+                # print(losses)
+
+            prediction = min(losses, key=losses.get)
+            y_pred.append(prediction)
+
+        print(f'predictions: {y_pred}')
+        print(f'gold label: {y_true}')
+        f_score = f1_score(y_true, y_pred, average=None)
+        print(f'f_score: {f_score}')
+
+        '''
+        classes_pred = list()
+        classes_true = list()
+        for pred, gold in zip(y_pred, y_true):
+            classes_pred.append(labels_emo_map_reversed[pred])
+            classes_true.append(labels_emo_map_reversed[gold])
+
+        print(classes_true)
+        '''
+        x_axis_labels = ['sad', 'ang', 'hap', 'neu'] # labels for x-axis
+        y_axis_labels = ['sad', 'ang', 'hap', 'neu']
+
+        #Get the confusion matrix
+        cf_matrix = confusion_matrix(y_true, y_pred)
+        print(cf_matrix)
+        svm = sns.heatmap(cf_matrix/np.sum(cf_matrix), annot=True, 
+            fmt='.2%', cmap='Blues', xticklabels=x_axis_labels, yticklabels=y_axis_labels)
+        figure = svm.get_figure()    
+        figure.savefig(confusion_file, dpi=400)
